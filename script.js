613c953fd64e679968a32b2a04e5b71c160f718b
interrowhimper <140921905+interrowhimper@users.noreply.github.com>
2023-07-29 18:47:52 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,83 @@
-// ... (previous code) ...
+// Pronouns with equal probability
+const pronouns = [
+  "I",
+  "You (singular)",
+  "You two",
+  "You 3+",
+  "We 2 (inclusive)",
+  "We 2 (exclusive)",
+  "We 3+ (inclusive)",
+  "We 3+ (exclusive)",
+  "She/he/it",
+  "They (2+)",
+];
+
+// Root words with equal probability
+const rootWords = ["adawo", "an(v)t", "aduli"];
+
+// Tense options with equal probability
+const tenseOptions = [
+  "Present",
+  "Completive Past (Reported)",
+  "Completive Past (Inexperienced)",
+  "Incompletive Past",
+  "Future",
+  "Future Progressive",
+  "Immediate",
+  "Habitual",
+];
+
+// Categories with equal probability
+const affixes = [
+  ["translocative", "cislocative"],
+  ["Irrealis"],
+  ["Relativizer"],
+  ["Causative"],
+  ["Applicative"],
+  ["Reversive"],
+  ["Ambulative"],
+  ["Completive"],
+];
+
+function drawWord(canvas, word) {
+  const ctx = canvas.getContext("2d");
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.font = "24px Arial";
+  ctx.textAlign = "center";
+  ctx.textBaseline = "middle";
+  ctx.fillText(word, canvas.width / 2, canvas.height / 2);
+}
+
+function getRandomElementFromArray(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
+
+function generatePronoun() {
+  const canvas = document.getElementById("pronounCanvas");
+  const pronoun = getRandomElementFromArray(pronouns);
+  drawWord(canvas, pronoun);
+}
+
+function generateRootWord() {
+  const canvas = document.getElementById("rootCanvas");
+  const rootWord = getRandomElementFromArray(rootWords);
+  drawWord(canvas, rootWord);
+}
+
+function generateTense() {
+  const canvas = document.getElementById("tenseCanvas");
+  const tense = getRandomElementFromArray(tenseOptions);
+  drawWord(canvas, tense);
+}
 
 function generateAffix() {
   const canvas = document.getElementById("affixCanvas");
   const maxAffixes = parseInt(document.getElementById("maxAffixesInput").value);
-  const selectedAffixes = Array.from(
-    document.querySelectorAll(".affixCheckbox:checked")
-  ).map((checkbox) => checkbox.value);
-
   let word = "";
 
-  // Generate random affixes based on the maximum allowed or selected affixes
+  // Generate random affixes based on the maximum allowed
   for (let i = 0; i < maxAffixes; i++) {
-    if (
-      selectedAffixes.length > 0 &&
-      (Math.random() <= 0.5 || i >= selectedAffixes.length)
-    ) {
-      const randomAffix = getRandomElementFromArray(selectedAffixes);
-      word += randomAffix + " ";
-    } else if (Math.random() <= 0.5) {
+    if (Math.random() <= 0.5) {
       const affix = getRandomElementFromArray(affixes);
       const affixWord = getRandomElementFromArray(affix);
       word += affixWord + " ";
@@ -51,15 +111,4 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const generateRandomButton = document.getElementById("generateRandomButton");
   generateRandomButton.addEventListener("click", generateRandomWord);
-
-  const drillModeCheckbox = document.getElementById("drillModeCheckbox");
-  drillModeCheckbox.addEventListener("change", () => {
-    const affixCheckboxes = document.querySelectorAll(".affixCheckbox");
-    affixCheckboxes.forEach((checkbox) => {
-      checkbox.disabled = drillModeCheckbox.checked;
-      if (drillModeCheckbox.checked) {
-        checkbox.checked = false;
-      }
-    });
-  });
 });
